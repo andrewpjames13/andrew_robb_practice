a952972cc130881a509920f8b8df1dc5b1e01213
Andrew James <andrewpjames87@gmail.com>
2015-02-10 14:50:49 -0700

    Add User create and edit functions

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,4 +4,36 @@ class CompaniesController < ApplicationController
     @companies = Company.all
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to companies_path
+    else
+      render ("new")
+    end
+  end
+
+  def edit
+    @company = Company.find(params[:id])
+  end
+
+  def update
+    @company = Company.find(params[:id])
+    if @company.update(company_params)
+      redirect_to companies_path
+    else
+      render ("edit")
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :suffix, :catch_phrase, :email, :start_date, :industry)
+  end
+
 end
